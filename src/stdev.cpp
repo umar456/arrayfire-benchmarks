2223e6a2b04ae068633c281bf1249b85b435b279
arrayfire <sb199898.sb@gmail.com>
2019-05-17 15:35:47 -0400

    Removed memory calc from stdev

--- src/stdev.cpp
+++ src/stdev.cpp
@@ -12,25 +12,14 @@ using af::sum;
 void stdevBench(benchmark::State& state, af_dtype type) {
     af::dim4 aDim(state.range(0), state.range(1));
     array a = randu(aDim, type);
-    unsigned stdevDim = state.range(2) - 1;
-    array output;
-    size_t alloc_bytes, alloc_buffers, lock_bytes, lock_buffers;
-    deviceMemInfo(&alloc_bytes, &alloc_buffers, &lock_bytes, &lock_buffers);
-    af::sync();
-
-    output = stdev(a, stdevDim);
+    unsigned stdevDim = state.range(2);
+    { stdev(a, stdevDim); }
     af::sync();
 
     for(auto _ : state) {
-        output = stdev(a, stdevDim);
+        stdev(a, stdevDim);
         af::sync();
     }
-
-    size_t alloc_bytes2, alloc_buffers2, lock_bytes2, lock_buffers2;
-    deviceMemInfo(&alloc_bytes2, &alloc_buffers2, &lock_bytes2, &lock_buffers2);
-
-    state.counters["Bytes"] = (alloc_bytes2 - alloc_bytes);
-    deviceGC(); 
 }
 
 int main(int argc, char** argv) {
@@ -38,7 +27,7 @@ int main(int argc, char** argv) {
 
     benchmark::Initialize(&argc, argv);
     af::benchmark::RegisterBenchmark("stdev", types, stdevBench)
-        ->Ranges({{8, 1<<12}, {8, 1<<12}, {0, 1}})
+        ->Ranges({{8, 1<<12}, {8, 1<<12}, {0, 3}})
         ->ArgNames({"dim0", "dim1", "stdevDim"})
         ->Unit(benchmark::kMicrosecond);
 
